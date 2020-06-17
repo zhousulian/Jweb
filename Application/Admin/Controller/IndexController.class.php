d1af2536c4a82b4f8271405fe531dfe87d783879
‘zhousulian’ <zhousulianzsl@163.com>
2020-06-17 14:49:40 +0800

    修改了一点细节信息

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -23,6 +23,22 @@ class IndexController extends BaseController
         $ser = $_SERVER;
         dump($ser);
     }
+    public function interl()
+    {
+        $b = $this->back_url;
+        dump($b);
+        $dl = $this->de_li_url;
+        dump($dl);
+
+
+        $url = $_SERVER['SCRIPT_NAME']; //根目录文件夹部分
+        dump($url);
+        //var_dump(strrpos($url, 'x', -1));
+        dump(substr_count($url, '/'));
+
+        $ser = $_SERVER;
+        dump($ser);
+    }
 
 }
 
